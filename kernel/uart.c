3a8b9d6ceb1988c8dfbe4c1b8618e6e941a8812d
remy <remyciterin@gmail.com>
2025-08-31 18:06:33 +0200

    clean uart.c

--- kernel/uart.c
+++ kernel/uart.c
@@ -15,55 +15,19 @@
 // address of one of the registers.
 #define Reg(reg) ((volatile unsigned char *)(UART0 + reg))
 
-// the UART control registers.
-// some have different meanings for
-// read vs write.
-// http://byterunner.com/16550.html
-#define RHR 0 // receive holding register (for input bytes)
-#define THR 0 // transmit holding register (for output bytes)
-#define IER 1 // interrupt enable register
-#define FCR 2 // FIFO control register
-#define ISR 2 // interrupt status register
-#define LCR 3 // line control register
-#define LSR 5 // line status register
-
 #define ReadReg(reg) (*(Reg(reg)))
 #define WriteReg(reg, v) (*(Reg(reg)) = (v))
 
 void
 uartinit(void)
-{
-  // // disable interrupts.
-  // WriteReg(IER, 0x00);
-
-  // // special mode to set baud rate.
-  // WriteReg(LCR, 0x80);
-
-  // // LSB for baud rate of 38.4K.
-  // WriteReg(0, 0x03);
-
-  // // MSB for baud rate of 38.4K.
-  // WriteReg(1, 0x00);
-
-  // // leave set-baud mode,
-  // // and set word length to 8 bits, no parity.
-  // WriteReg(LCR, 0x03);
-
-  // // reset and enable FIFOs.
-  // WriteReg(FCR, 0x07);
-
-  // // enable receive interrupts.
-  // WriteReg(IER, 0x01);
-}
+{}
 
 // write one output character to the UART.
 void
 uartputc(int c)
 {
-  // wait for Transmit Holding Empty to be set in LSR.
-  //while((ReadReg(LSR) & (1 << 5)) == 0)
-  //  ;
-  WriteReg(THR, c);
+  while (0x01 & ~ReadReg(1)) {}
+  WriteReg(0, c);
 }
 
 // read one input character from the UART.
@@ -72,7 +36,7 @@ int
 uartgetc(void)
 {
   if (ReadReg(2) & 0x01) {
-    return ReadReg(RHR);
+    return ReadReg(0);
   } else {
     return -1;
   }
